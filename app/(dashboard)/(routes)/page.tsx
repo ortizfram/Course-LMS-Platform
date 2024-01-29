42031e560ae3ec8d42851afb436d34b039ca3284
melon <ortizfranco48@gmail.com>
2024-01-29 17:07:16 -0300

    user data

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
+    <>
+    <UserButton />
     <p>Home unprotected</p>
+    </>
   );
 }
