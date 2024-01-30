e27d31dfb4a9490812049ff13db67c88b5f28df1
ortizfram <ortizfranco48@gmail.com>
2024-01-30 12:14:30 -0300

    create course title

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,13 +8,13 @@ const DashboardLayout = ({
 }) => {
     return ( 
         <div className="h-full">
-        <div className="h-[80] md:pl-56 fixed inset-y-0 w-full z-50">
+        <div className="h-[80px] md:pl-[56px] fixed inset-y-0 w-full z-50">
             <Navbar />
         </div>
             <div className="hidden md:flex h-full w-56 felx-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
-            <main className="md:pl-56 h-full">
+            <main className="md:pl-56 pt-[80px] h-full">
             {children}
             </main>
         </div>
