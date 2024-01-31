287cf525bcee3aa36c7475399d7929e32419e8e7
ortizfram <ortizfranco48@gmail.com>
2024-01-31 09:33:47 -0300

    first prisma schema

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,10 +4,11 @@ import * as z from "zod";
 import axios from 'axios';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 import {
     Form,
@@ -26,6 +27,7 @@ const formSchema = z.object({
 });
 
 const CreatePage = () => {
+    const router = useRouter()
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -35,8 +37,13 @@ const CreatePage = () => {
 
     const { isSubmitting, isValid } = form.formState;
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await axios.post("/api/course", values)
+            router.push(`/teacher/courses/${response.data.id}`)
+        } catch (error) {
+            toast.error("Something went wrong")
+        }
     };
 
     return (
